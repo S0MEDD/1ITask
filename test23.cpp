63a15662ae9b69874f0525fed526a8182dcb9d94
S0MEDD <126161457+S0MEDD@users.noreply.github.com>
2023-10-11 10:56:52 +0300

    Update test23.cpp

--- test23.cpp
+++ test23.cpp
@@ -4,25 +4,32 @@
 
 int main()
 {
-    Person A;
-    string N, S;
-    int W, n, e;
-    vector <int> temp;
-    float Fin;
-    cout << "Please input Person name and surname";
-    cin >> N >> S;
-    cout << "Please input how many works has Person ";
-    cin >> n;
-    for (int i = 0; i < n; i++) 
-    {
-        cout << "Please input work " << i + 1 << "evaluation point: ";
-        cin >> W;
-        temp.push_back(W);
+    vector <Person> Group;
+    for (int j = 0; j < 2; j++) {
+        string N, S;
+        int W, n, e;
+        vector <int> temp;
+        float Fin;
+        cout << "Please input Person name and surname";
+        cin >> N >> S;
+        cout << "Please input how many works has Person ";
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Please input work " << i + 1 << "evaluation point: ";
+            cin >> W;
+            temp.push_back(W);
+        }
+        cout << "Please input final exam result: ";
+        cin >> E;
+        Fin = 0.4 * (std::accumulate(temp.begin(), temp.end(), 0.0) / temp.size()) + 0.6 * E;
+        Person A(N, S, temp, E, Fin);
+        Group.push_back(A);
+        A.~Person();
+        temp.clear();
     }
-    cout << "Please input final exam result: ";
-    cin >> E;
-    Fin = 0.4 * (std::accumulate(temp.begin(), temp.end(), 0.0) / temp.size()) + 0.6 * E;
-    Person A(N, S, temp, E, Fin);
+    for (auto& B : Group) B.printPerson();
+    system("pause");
 
-    A.printPerson();
+  
 }
