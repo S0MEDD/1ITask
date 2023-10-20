0cc959c5ee50e3729535fd26c7fe994ec0568ae9
S0MEDD <126161457+S0MEDD@users.noreply.github.com>
2023-10-20 14:07:03 +0300

    new version

--- Mylib1.h
+++ Mylib1.h
@@ -4,6 +4,9 @@
 #include <string>
 #include <vector>
 #include <numeric>
+#include <chrono>
+#include <random>
+
 
 using std::cout;
 using std::cin;
@@ -15,6 +18,10 @@ using std::left;
 using std::right;
 using std::fixed;
 using std::setprecision;
+using std::mt19937;
+
+using hrClock = std::chrono::high_resolution_clock; 
+typedef std::uniform_int_distribution<int> int_distribution;
 
 int minimum(int a, int b);
 
@@ -25,15 +32,18 @@ class Person {
 	float Final;
 public:
 	Person();
-	Person(string N, string S, vector <int> H, int E, float F);
-
+	Person(string N, string S, 
+		vector <int> H, int E, float F);
 	Person& operator=(const Person& P);
 	Person(const Person& P);
 	~Person();
 	///setters
-	inline void setName(string N) { Name = N; }
+	inline void setName(string N) { 
+		Name = N; }
 	///overloaded cin
 	///overloaded cout
+	friend std::istream& operator>>(std::istream& in, Person &A);
 	/// final point calculation from average or median
+	friend std::ostream& operator<<(std::ostream& out, const Person& A);
 	void printPerson();
 };
